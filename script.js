3c8706065113bed97059407b3ccd98a305d44eef
solomonjemberu818-del <solomonjemberu818@gmail.com>
2026-02-14 10:11:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let myColor=document.getElementById("color");
+let myHead=document.getElementById("head");
+let myBtn=document.getElementById("btn");
+myBtn.addEventListener("click",function(){
+    myHead.innerText="sekaramochi endatiwediku teregagu";
+    myHead.style.background="green";
+    myHead.style.color="white";
+   document.body.style.backgroundColor=myColor.Value;
+
+ 
+
+});
+let myText=document.getElementById("text");
